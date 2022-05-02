34384bbe24341dc758266b35dbec2d94037035ed
Robson Melo de Souza <sys.robson@gmail.com>
2022-05-02 05:33:29 -0300

    Design concluído

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import './Contact.css'
+import linkedin from "../images/linkedin.png";
+import facebook from "../images/facebook.png";
+import instagram from "../images/instagram.png";
+import whatsapp from "../images/whatsapp.png";
+import github from "../images/github.png";
 
 const Contact = () => {
   return (
     <>
-    <h1 className='contact-title'>Vamos Trabalhar Juntos!</h1>
     <div className='contact-container'>
-  
-       <article>
+           <article>
+           <h1 className='contact-title'>Vamos Trabalhar Juntos!</h1>
            <p>
                Preencha o formulário de contato ou envie um email para <br/>
-               <span className='email'>sys.robson@gmail.com</span>
+               <span className='email'>sys.robson@gmail.com</span>.<br/>
+               Minhas redes sociais:
            </p>
+           <a href='https://www.linkedin.com/in/robsonthedev/'><img src={linkedin} alt="" className='social-icon'/></a>
+           <a href='https://github.com/robson-melo-dev'><img src={github} alt=""className='social-icon'/></a>
+           <a href='https://www.instagram.com/robson.raven/'><img src={instagram} alt=""className='social-icon'/></a>
+           <a href='https://www.facebook.com/robsonpantro'><img src={facebook} alt=""className='social-icon'/></a>
+           <a href='https://api.whatsapp.com/send?phone=5555981182976&text=Ol%C3%A1!%20Vim%20pelo%20Portf%C3%B3lio.'><img src={whatsapp} alt=""className='social-icon'/></a>
+           
        </article>
        <form className="contact-form">
            <label>
-           <input type="text" placeholder='Nome'/>
+           <input type="text" placeholder='Nome'className='input'/>
            </label>
            <label>           
-               <input type="text" placeholder='Email'/>
+               <input type="email" placeholder='Email'className='input'/>
             </label>
            <label>
-           <input type="text" placeholder='Assunto'/>
+           <input type="text" placeholder='Assunto'className='input'/>
            </label>
-           <label>
-           <input type="text" placeholder='Mensagem'/>
+           <label className='input-message'>
+           <textarea placeholder='Mensagem'/>
            </label>
            <label>
            <input type="submit" value="Enviar"/>
            </label>
            
        </form>
-    </div>
+       </div>
+       <div className="next-page">
+        <span className="next-page-btn">
+          <a href="projects">&#10094; Projetos</a>
+        </span>
+        </div>
     </>
   )
 }
